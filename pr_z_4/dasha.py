bb05c6008b3813f39aa935742a6198bf5ee1f98a
Aku52 <kotik.52@tutamail.com>
2025-11-28 11:43:04 +0300

    Add files via upload

--- pr_z_4/dasha.py
+++ pr_z_4/dasha.py
@@ -1,4 +1,8 @@
-# Построить модель из 3 Dense-слоёв и вывести формы весов каждого
+# Построить модель сохранить веса обучить одну
+#  эпоху снова сохранить. Сравнить, какие веса изменились сильнее.
+
+
+# Импорт библиотек
 import numpy as np
 from keras import models, layers
 from keras.datasets import mnist
@@ -9,7 +13,7 @@ from keras.utils import to_categorical
 x_train = x_train[:200].reshape((200, 28*28)).astype("float32") / 255
 y_train = y_train[:200]
 
-
+# Создание модели
 model = models.Sequential([
     layers.Dense(3, activation='relu', input_shape=(28*28,)),
     layers.Dense(32, activation='relu'),
@@ -21,16 +25,19 @@ model.compile(
     loss='sparse_categorical_crossentropy'
 )
 
+# Первое сохранение весо
 w_before, b_before = model.layers[0].get_weights()
-
+# Тренировка весов
 model.fit(
         x_train, y_train, 
-        epochs=3, 
+        epochs=1, 
         batch_size=32, 
         verbose=1)
-
+# Второе сохранение весов
 w_after, b_after = model.layers[0].get_weights()
+delta_w = w_after -w_before
+# Сравнение и сохранение
+print("Среднее изменение весов:", np.mean(np.abs(delta_w)))
+print("Максимальное изменение весов:", np.max(np.abs(delta_w)))
 
-for i,layer in enumerate (model.layers):
-    w, b = layer.get_weights()
-    print(f"Слой {i}: форма весов {w.shape}")
+
