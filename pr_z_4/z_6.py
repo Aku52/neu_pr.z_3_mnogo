75e293bcb57a389eb6e93394e39de96a5ecac296
Aku52 <kotik.52@tutamail.com>
2025-12-01 15:48:23 +0300

    Add files via upload

--- pr_z_4/z_6.py
+++ pr_z_4/z_6.py
@@ -1,35 +1,46 @@
+# Построить модель из 3 Dense-слоёв и вывести формы весов каждого
+# Усложнение:Сравнить, как изменится форма при другом размере входного вектора
 import numpy as np
-from keras import models,layers 
-from keras.datasets import mnist 
+from keras import models, layers
+from keras.datasets import mnist
 from keras.utils import to_categorical
+import tensorflow as tf
 
-(x_train, y_train), (x_test,y_test) = mnist.load_data() 
-x_train = x_train[:200].reshape((200, 28*28)).astype("float32")/255 
-x_test =to_categorical(y_train[:200])
-#x_train[:200]- первые 200 эл, reshape((200, 28*28))-размер,astype("float32") - приводение к типу
 
-model= models.Sequential([
-        layers.Dense(64, activation='relu',input_shape=(28*28,)), 
-        layers.Dense(10,activation='softmax') 
-    ])
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# уменьшаем каждое изображение до 24x24
+x_train = tf.image.resize(x_train[..., tf.newaxis], (24, 24)).numpy()
+
+x_train= x_train[:200].reshape((200, 24*24)).astype("float32") / 255
+
+y_train = y_train[:200]
+
+
+model = models.Sequential([
+    layers.Dense(3, activation='relu', input_shape=(24*24,)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
 
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy'
-    )
+)
 
-w_before,b_before= models.layers[0].get_weights()
+w_before, b_before = model.layers[0].get_weights()
 
-# На каждой эпохе веса меняются 
 model.fit(
         x_train, y_train, 
-        epochs=1, 
+        epochs=3, 
         batch_size=32, 
         verbose=1)
 
-w_after,b_after = models.layers[0].get_weights()
-delta_w = w_after -w_before
+w_after, b_after = model.layers[0].get_weights()
 
-print("Максимальное значение веса:",np.max(np.abs(delta_w)))
-print("Среднее значение веса:",np.mean(np.abs(delta_w))) 
-# np.mean(np.abs(delta_w)), abs- модуль,mean-среднее арифмитическое, delta_w- изменения
+for i,layer in enumerate (model.layers):
+    w, b = layer.get_weights()
+    print(f"Слой {i}: форма весов {w.shape}")
+
+print (w_before.shape)
+print (b_before.shape)
